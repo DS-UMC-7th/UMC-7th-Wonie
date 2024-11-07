0bad091e70754628911d2549e612b0df021de1a0
heeone1 <rinakim1019@gmail.com>
2024-11-08 02:33:59 +0900

    navbar 수정

--- movie site/src/pages/login2.jsx
+++ movie site/src/pages/login2.jsx
@@ -31,6 +31,8 @@ const login = () => {
             localStorage.setItem('accessToken', response.data.accessToken);
             localStorage.setItem('refreshToken', response.data.refreshToken);
             
+            window.dispatchEvent(new Event('loginStatusChanged'));
+
             navigate('/');
         } catch (error) {
             console.error('로그인 실패:', error.response ? error.response.data : error.message);
@@ -39,17 +41,24 @@ const login = () => {
 
     return (
         <Container>
+            <StyledTitle>회원가입</StyledTitle>
+
             <Input error={login.touched.email && login.errors.email} type={"email"} placeholder={"이메일을 입력해주세요!"} {...login.getTextInputProps("email")}/>
             {login.touched.email && login.errors.email && <ErrorText>{login.errors.email}</ErrorText>}
             <Input error={login.touched.password && login.errors.password} type={"password"} placeholder={"비밀번호를 입력해주세요!"} {...login.getTextInputProps("password")}/>
             {login.touched.password && login.errors.password && <ErrorText>{login.errors.password}</ErrorText>}
 
-            <button onClick={handlePressLogin}>로그인</button>
+            <StyledButton onClick={handlePressLogin}>로그인</StyledButton>
         </Container>
     );
 };
 
 export default login;
+
+const StyledTitle = styled.h1`
+    color: white;
+    margin-top: 5px;
+`;
 
 const Container = styled.div`
     display: flex;
@@ -76,4 +85,20 @@ const Input = styled.input`
 const ErrorText = styled.h1`
     color: red;
     font-size: 12px;
+`;
+
+const StyledButton = styled.button`
+    padding: 10px 20px;
+    font-size: 16px;
+    width: 315px;
+    cursor: pointer;
+    background-color: #ea195c;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    margin: 10px 0;
+
+    &:hover {
+        background-color: #ce1752;
+    }
 `;
